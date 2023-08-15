e2e596cdd33c6ef8bd666388fd046a59479cd19f
Raphael de Mello - RDM <raph.mello@gmail.com>
2023-08-15 15:51:32 -0300

    Slices and 'type'

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
+//var deckSize int
 
-var deckSize int
+//func main() {
+//	//declaring variables
+//	var card string = "Ace of Spades" //creating and assigning
+//	card1 := "Ace of Spades"          //creating and assigning
+//	card1 = "Five of Diamonds"        //only assigning
+//	fmt.Println(card)
+//	fmt.Println(card1)
+//	deckSize = 10
+//	fmt.Println(deckSize)
+//}
+
+//func main() {
+//	card := newCard()
+//	fmt.Println(card)
+//
+//}
+//
+//func newCard() string {
+//	printState()
+//	return "Five of Diamonds"
+//}
 
 func main() {
-	//declaring variables
-	var card string = "Ace of Spades" //creating and assigning
-	card1 := "Ace of Spades"          //creating and assigning
-	card1 = "Five of Diamonds"        //only assigning
-	fmt.Println(card)
-	fmt.Println(card1)
-	deckSize = 10
-	fmt.Println(deckSize)
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	cards.print()
+	printState()
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
